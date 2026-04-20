e75840702cafe5e0db4033cc0100dc4220431def
Dr. Younis <lamajawad@DrYounis-3.local>
2026-04-20 23:16:41 +0300

    feat: added new phonics words (sky, fly, cry, baby) to dynamic dictation engine

--- components/dictation/dictionary.js
+++ components/dictation/dictionary.js
@@ -1,10 +1,17 @@
 export const dictationWords = [
+    // Original 'ee' phonics list
     { word: 'bee', emoji: '🐝', translation: 'نحلة' },
     { word: 'sheep', emoji: '🐑', translation: 'خروف' },
     { word: 'knee', emoji: '🦵', translation: 'ركبة' },
     { word: 'wheel', emoji: '🛞', translation: 'عجلة' },
     { word: 'jeep', emoji: '🚙', translation: 'جيب' },
-    { word: 'teeth', emoji: '🦷', translation: 'أسنان' }
+    { word: 'teeth', emoji: '🦷', translation: 'أسنان' },
+
+    // New 'y' phonics list added from worksheet image
+    { word: 'sky', emoji: '☁️', translation: 'سماء' },
+    { word: 'fly', emoji: '🦸', translation: 'يطير' },
+    { word: 'cry', emoji: '😭', translation: 'يبكي' },
+    { word: 'baby', emoji: '👶', translation: 'طفل' }
 ];
 
 export const playAudio = (word) => {
